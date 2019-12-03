164302b0ec7b782273b76f055775ebb53f45173a
Juan Lunar <juanjlunarc@gmail.com>
2019-12-02 22:04:11 -0400

    Creating Empty default HomeView

---
+++ app/src/components/HomeView.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class HomeView extends React.Component {
+    render() {
+        return (
+            <main>
+                
+            </main>
+        );
+    }
+}
+export default HomeView;
