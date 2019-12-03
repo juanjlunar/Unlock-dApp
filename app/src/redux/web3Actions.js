28345dbe6fb2d0420443b457868d2581fee95f4b
Juan Lunar <juanjlunarc@gmail.com>
2019-12-03 13:44:02 -0400

    adding web3 actions and web3 reducers into redux
    adding web3 reducer to redux store
    conecting root component to redux to store initial web3 configuration and contract

---
+++ app/src/redux/web3Actions.js
@@ -0,0 +1,30 @@
+export const actions = {
+    STORE_MAIN_ACCOUNT: 'STORE_MAIN_ACCOUNT',
+    CHANGE_MAIN_ACCOUNT: 'CHANGE_MAIN_ACCOUNT',
+    SHOW_METAMAST_INSTALL_MESSAGE: 'SHOW_METAMAST_INSTALL_MESSAGE',
+    GET_CONTRACT_ACTIONS: 'GET_CONTRACT_ACTIONS'
+};
+
+export function storeMainAccount(ethereumEnabler) {
+    return {
+        type: actions.STORE_MAIN_ACCOUNT,
+        payload: ethereumEnabler
+    };
+}
+export function changeMainAccount(account) {
+    return {
+        type: actions.CHANGE_MAIN_ACCOUNT,
+        account
+    };
+}
+export function showMetamaskInstallMessage() {
+    return {
+        type: actions.SHOW_METAMAST_INSTALL_MESSAGE
+    };
+}
+export function getContractActions(contractPromise) {
+    return {
+        type: actions.GET_CONTRACT_ACTIONS,
+        payload: contractPromise
+    };
+}
