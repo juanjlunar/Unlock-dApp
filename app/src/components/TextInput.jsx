35561747255133d03706bedf424fced45100d424
Juan Lunar <juanjlunarc@gmail.com>
2019-12-04 11:55:43 -0400

    Adding TextInput component to handle user input and UpLoadFile form component bug fix

---
+++ app/src/components/TextInput.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+// importing styles
+import './styles/TextInput.css';
+
+class TextInput extends React.Component {
+    constructor() {
+        super();
+        this.onChange = this.onChange.bind(this);
+        this.getFileInputRef = this.getFileInputRef.bind(this);
+        this.focusInput = this.focusInput.bind(this);
+    }
+    onChange(e) {
+        if (typeof this.props.onChange) {
+            this.props.onChange(e);
+        }
+    }
+    getFileInputRef(el) {
+        if (el) {
+            this.inputRef = el;
+        }
+    }
+    focusInput() {
+        this.inputRef.focus();
+    }
+    renderInputByType() {
+        const { className, required } = this.props;
+        if (this.props.type === 'textarea') {
+            return (
+                <textarea 
+                    ref={this.getFileInputRef} 
+                    onChange={this.onChange}
+                    {...Object.assign({}, required ? {required} : {})}
+                />
+            );
+        }
+        return (
+            <input 
+                ref={this.getFileInputRef} 
+                onChange={this.onChange} 
+                type="text"
+                {...Object.assign({}, this.props.className ? {className} : {})}
+                {...Object.assign({}, required ? {required} : {})}
+            />
+        );
+    }
+    render() {
+        console.log('TextInput')
+        return (
+            <div className="text-input-container flex-c-y-centered">
+                <div className="full-width flex-container">
+                    <label onClick={this.focusInput}>{this.props.label || 'Input label'}</label>
+                </div>
+                {this.renderInputByType()}
+            </div>
+        );
+    }
+}
+export default TextInput;
